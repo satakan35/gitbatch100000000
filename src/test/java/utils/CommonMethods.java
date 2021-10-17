3cfc1df5d1fa7cb35e81438597be79c027d93033
sohel123 <sohel.abbasi@yash.com>
2021-10-18 00:27:53 +0530

    Latest code 10182021

---
+++ src/test/java/utils/CommonMethods.java
@@ -0,0 +1,42 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class CommonMethods {
+
+    public static WebDriver driver;
+
+    @BeforeMethod(alwaysRun = true)
+    public void openBrowser(){
+        ConfigReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+        switch (ConfigReader.getPropertyValue("browser")) {
+            case "chrome":
+              //  System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver.exe");
+               WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            default:
+                throw new RuntimeException("Invalid browser name");
+        }
+        driver.get(ConfigReader.getPropertyValue("url"));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
